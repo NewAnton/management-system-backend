e86a67b896ea241c33dcd4c22b4147a81e94af56
NewAnton <anton1korobov1@gmail.com>
2022-11-23 06:41:33 +0300

    init: add database information

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,20 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = 'anton';
+const Password = '123456';
+const clasterInfo = 'cluster0.vzjy1uk';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/project-management-app`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
